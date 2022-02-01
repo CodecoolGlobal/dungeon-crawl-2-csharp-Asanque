924ce3885085cc43eddd4a994678a5709ea42624
Asanque <kis.gergely93@gmail.com>
2022-02-01 16:00:04 +0100

    add special sprites

---
+++ Assets/Source/Actors/Static/WallSpecial.cs
@@ -0,0 +1,16 @@
+﻿using DungeonCrawl.Actors.Characters;
+
+namespace DungeonCrawl.Actors.Static
+{
+    public class WallSpecial : Wall
+    {
+        public override bool OnCollision(Actor anotherActor)
+        {
+            if (anotherActor.GetType() == typeof(Skeleton))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
