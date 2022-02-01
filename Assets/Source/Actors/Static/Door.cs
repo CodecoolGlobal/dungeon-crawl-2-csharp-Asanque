50a41e7f760acd38bf412828b469c6166d815d9e
Asanque <kis.gergely93@gmail.com>
2022-02-01 10:42:39 +0100

    initialise Door class, implement logic

---
+++ Assets/Source/Actors/Static/Door.cs
@@ -0,0 +1,28 @@
+﻿using DungeonCrawl.Actors.Characters;
+
+namespace DungeonCrawl.Actors.Static
+{
+    public class Door : Actor
+    {
+        private bool isOpen = false;
+        public override bool OnCollision(Actor anotherActor)
+        {
+            if (anotherActor.GetType() == typeof(Skeleton))
+            {
+                return false;
+            }
+            if (!isOpen)
+            {
+                if (anotherActor.HasKey())
+                {
+                    isOpen = true;
+                    return true;
+                };
+                return false;
+            }
+            return true;
+        }
+        public override int DefaultSpriteId => 485;
+        public override string DefaultName => "Door";
+    }
+}
