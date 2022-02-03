fcf64fc2ca351c3937fd2246300ed61c80abb4f3
Gargameth <stragorhelm@gmail.com>
2022-02-03 11:24:43 +0100

    Added music

---
+++ Assets/Source/Sounds/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Source.Sounds
+{
+    public class PlayAudio: MonoBehaviour
+    {
+        public AudioSource audioSource;
+        void Start()
+        {
+            audioSource.Play();
+        }
+
+    }
+}
